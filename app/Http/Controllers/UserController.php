20d94bcdda12a716fa00298cadfe415a8b94d888
KristapsRokis <kristaps092@gmail.com>
2024-01-01 23:34:59 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
